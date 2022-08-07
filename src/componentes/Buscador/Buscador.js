868c239e7daebec6b9669586478b43ef0264fed3
JimenaKatuMartinez <93katu@gmail.com>
2022-08-07 19:48:09 -0300

    Estilos aplicados a search-bar

--- src/componentes/Buscador/Buscador.js
+++ src/componentes/Buscador/Buscador.js
@@ -1,10 +1,12 @@
 import './buscador.scss';
+import { BsBag } from 'react-icons/bs';
 
 const Buscador = () => {
   return (
     <div className='wrapper-buscador'>
         <div className="wrapper-input">
-            <input type="search" placeholder='Title, companies, expertise or benefits' />
+            <label htmlFor="search-job" id='bag-label-icon'><BsBag/></label>
+            <input type="search" id='search-job' placeholder='Title, companies, expertise or benefits' />
             <button type="submit"className='button-search' >Search</button>
         </div>
     </div>
