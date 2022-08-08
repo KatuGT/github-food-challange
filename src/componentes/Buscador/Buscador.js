d849cdb8d0d68ce4f417a7695da87c7ceb0f005a
JimenaKatuMartinez <93katu@gmail.com>
2022-08-08 12:30:44 -0300

    Focus-within a form agregado

--- src/componentes/Buscador/Buscador.js
+++ src/componentes/Buscador/Buscador.js
@@ -4,11 +4,11 @@ import { BsBag } from 'react-icons/bs';
 const Buscador = () => {
   return (
     <div className='wrapper-buscador'>
-        <div className="wrapper-input">
+        <form className="wrapper-input">
             <label htmlFor="search-job" id='bag-label-icon'><BsBag/></label>
             <input type="search" id='search-job' placeholder='Title, companies, expertise or benefits' />
             <button type="submit"className='button-search' >Search</button>
-        </div>
+        </form>
     </div>
   );
 };
