e5a67f63430a473f09849de0a4a2086c351d85dc
JimenaKatuMartinez <93katu@gmail.com>
2022-08-14 12:40:39 -0300

    homepage para desktop lista

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import Buscador from './componentes/Buscador/Buscador';
 import Filtro from './componentes/Filtro/Filtro';
 import Header from './componentes/Header/Header';
 import ListaCards from './componentes/ListaCards/ListaCards';
+import Main from './componentes/Main/Main';
 import { QueryProvider } from './providers/queryProviders';
 
 function App() {
   return (
-   <QueryProvider>
-    <div className='App'>
-      <Header />
-      <Buscador />
-      <Filtro />
-      <ListaCards/>
-    </div>
-   </QueryProvider>
+    <QueryProvider>
+      <div className='App'>
+        <Header />
+        <Buscador />
+        <Main>
+          <Filtro />
+          <ListaCards />
+        </Main>
+      </div>
+    </QueryProvider>
   );
 }
 
--- src/componentes/Card/Card.js
+++ src/componentes/Card/Card.js
@@ -4,16 +4,23 @@ import { FiClock } from 'react-icons/fi';
 import NotFound from '../../assets/imagenes/notfound.png';
 
 const Card = ({
-  img, companyName, position, scheduleType, location, postedAt,
+  img,
+  companyName,
+  position,
+  scheduleType,
+  location,
+  postedAt,
 }) => {
   return (
     <div className='card'>
       <img src={!img ? NotFound : img} alt='logo de ' />
       <div className='info-wrapper'>
-        <h4 className='nombre-empresa'>{companyName}</h4>
-        <p className='puesto'>{position}</p>
-        <p className='tiempo'>{scheduleType}</p>
-        <div className='info-extra'>
+        <div className='columna-uno'>
+          <h4 className='nombre-empresa'>{companyName}</h4>
+          <p className='puesto'>{position}</p>
+          <p className='tiempo'>{scheduleType}</p>
+        </div>
+        <div className='info-extra columna-dos'>
           <div className='info'>
             <BiWorld className='icon-info' /> <p className='pais'>{location}</p>
           </div>
